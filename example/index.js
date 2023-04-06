2b9ea89e8082175d7ea35abef876302a028a2f42
Seyacat <seyacat@gmail.com>
2023-04-05 22:28:07 -0500

    Insert advance

--- example/index.js
+++ example/index.js
@@ -1,30 +1,48 @@
 import MapDb from '../mapdb.js';
 
-const log = console.log.bind(
-  console,
-  '%c %s',
-  'background: green; color: white'
-);
-
 const mdb = new MapDb();
 
 const users = mdb.createTable('users');
 try {
-  const users = mdb.createTable('users');
+  //ERROR DUPLICATED TABLE
+  mdb.createTable('users');
 } catch (e) {
-  log(e.message);
+  console.log(e.message);
 }
 const user1 = users.insert({ hola: 1 });
 const user2 = users.insert({ id: 'test', hola: 1 });
-log(users.describe());
-log(user1, user2);
+console.log(users.describe());
+console.log(user1, user2);
 
 const emails = mdb.createTable('emails', { id: 'email' });
 try {
-  const email1 = emails.insert({ perro: 1 });
+  //ERROR NO ID
+  emails.insert({ perro: 1 });
 } catch (e) {
-  log(e.message);
+  console.log(e.message);
 }
 const email1 = emails.insert({ email: 1 });
-log(emails.describe());
-log(email1);
+console.log(emails.describe());
+console.log(email1);
+
+const games = mdb.createTable('games',{unique:["name"]});
+
+const game1 = games.insert( {name:"juego1",desc:"j1"} )
+try {
+  //DUPLICATE FIELD
+games.insert( {name:"juego1",desc:"j2"} )
+} catch(e){console.log(e.message);}
+const game2 = games.insert( {name:"juego2",desc:"j1"} )
+console.log(games.describe());
+console.log("games",game1,game2)
+
+const rooms = mdb.createTable('rooms',{unique:["name"],hasOne:[{table:"games",field:"game"}]});
+const room1 = rooms.insert( {} )
+try{
+  const room2 = rooms.insert( {} )
+} catch(e){console.log(e.message);}
+
+console.log(room1)
+
+//console.log(mdb.describe())
+
--- mapdb.js
+++ mapdb.js
@@ -1,30 +1,44 @@
 import Crypto from 'crypto';
 
 export default class {
-  mdb = new Map();
+  tables = new Map();
   constructor() {}
+  describe() {
+    return { tables: [...this.tables.keys()] };
+  }
   createTable(tablename, options) {
     //CHECK TABLE EXISTS
-    if (this.mdb.has(tablename)) {
+    if (this.tables.has(tablename)) {
       throw new Error(`Table ${tablename} already exists`);
     }
-    const newtable = new Table(tablename, options);
-    this.mdb.set(tablename, newtable);
+    const newtable = new Table(this, tablename, options);
+    this.tables.set(tablename, newtable);
     return newtable;
   }
 }
 class Table {
+  mdb;
   name;
   id = 'id';
   data = new Map();
-  constructor(tablename, options) {
+  unique = new Map();
+  constructor(mdb, tablename, options) {
+    this.mdb = mdb
     this.name = tablename;
     if (options?.id) {
       this.id = options?.id;
     }
+    //CREATE UNIQUE MAPS
+    for(const uniqueField of options?.unique ?? [] ){
+      this.unique.set(uniqueField,new Map())
+    }
+    //CREATE ONE-MANY RELATION
+    for(const hasOneField of options?.hasOne ?? [] ){
+      this.mdb.createTable(`${hasOneField.table}-${this.name}`)
+    }
   }
   describe() {
-    return { id: this.id, name: this.name };
+    return { id: this.id, name: this.name, unique: [...this.unique.keys()] };
   }
   insert(record) {
     if (typeof record != 'object') {
@@ -42,10 +56,22 @@ class Table {
     if (this.data.has(record[this.id])) {
       throw new Error(`Record ${this.id} already exists`);
     }
+    //CHECK UNIQUE
+    for( const [key,val] of this.unique ){
+      if( val.has( record[key] ) ){
+        throw new Error(`Record '${key}' duplicated. ${key}:${record[key]}`);
+      }
+    }
     //STORE RECORD
     this.data.set(record[this.id], record);
+    //UPDATE UNIQUES
+    for( const [key,val] of this.unique ){
+      val.set( record[key] )
+    }
+    //TODO fill oneMany relationship
     return record;
   }
+  
 }
 
 function randomHexString(size = 40) {
